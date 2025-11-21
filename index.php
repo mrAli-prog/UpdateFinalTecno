e261daa3d2db2717598136c2d4f65fe30e4e5cab
Ali.php <163858431+mrAli-prog@users.noreply.github.com>
2025-11-21 08:48:53 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="assets/css/fontiran.css">
+    <link rel="stylesheet" href="../assets/css/fontiran.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css"
     integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <title>Document</title>
 </head>
 
@@ -869,9 +869,9 @@
                 </div>
             </div>
         </footer>
-    <script src="./assets/js/app.js"></script>
-    <script src="./assets/js/cart_handler.js"></script>
-    <script src="./assets/js/Timer.js"></script>
+    <script src="../assets/js/app.js"></script>
+    <script src="../assets/js/cart_handler.js"></script>
+    <script src="../assets/js/Timer.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', () => {
             const categoriesContainer = document.querySelector('.categories-container');
